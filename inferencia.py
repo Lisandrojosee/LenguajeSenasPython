ec571c6c3cbf6f0eed0b839194bd4a9d591a5b42
LISANDRO GARCIA <163014064+Lisandrojosee@users.noreply.github.com>
2026-03-03 19:59:35 -0400

    Add files via upload

--- inferencia.py
+++ inferencia.py
@@ -11,8 +11,9 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-#leer nuestro modelo
-model = YOLO ('letras.pt')
+
+# leer nuestro modelo
+model = YOLO('letras.pt')
 
 
 # Declarar Detector
@@ -22,33 +23,35 @@ while True:
     # Realizar la lectura de la captura
     ret, frame = cap.read()
 
-    #Extraer informacion de la mano
-    frame = detector.encontrarmanos(frame, dibujar= False)
+    # Extraer informacion de la mano
+    frame = detector.encontrarmanos(frame, dibujar=False)
 
-    #posicion de una sola mano
-    lista1, bbox, mano = detector.encontrarposicion(frame, ManoNum=0, dibujarPuntos=False, dibujarBox= False, color=[0, 0,255])
+    # posicion de una sola mano
+    lista1, bbox, mano = detector.encontrarposicion(
+        frame, ManoNum=0, dibujarPuntos=False, dibujarBox=False, color=[0, 0, 255])
 
-    #si hay mano
+    # si hay mano
     if mano == 1:
-        #extraer informacion
+        # extraer informacion
         xmin, ymin, xmax, ymax = bbox
 
-        #Asignamos Margen
+        # Asignamos Margen
         xmin = xmin - 40
         ymin = ymin - 40
         xmax = xmax + 40
         ymax = ymax + 40
 
-        #Relizar el recorte de nuestra mano
+        # Relizar el recorte de nuestra mano
         recorte = frame[ymin:ymax, xmin:xmax]
 
-        #Redimencionamiento
-        recorte = cv2.resize(recorte, (640, 640), interpolation = cv2.INTER_CUBIC)
+        # Redimencionamiento
+        recorte = cv2.resize(recorte, (640, 640),
+                             interpolation=cv2.INTER_CUBIC)
 
-        #Extraer resultados
+        # Extraer resultados
         resultados = model.predict(recorte, conf=0.55)
-        if len(resultados) !=0:
-            #iteramos
+        if len(resultados) != 0:
+            # iteramos
             for results in resultados:
                 masks = results.masks
             coordenadas = masks
@@ -56,10 +59,6 @@ while True:
             anotaciones = resultados[0].plot()
 
             cv2.imshow('RECORTE', anotaciones)
-
-
-
-
 
     cv2.imshow("LENGUAJE DE SEÑAS", frame)
     # Leer teclado
